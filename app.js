d40fe5bedea89a32835bb01f1f4983d201922e06
jakcharvat <jakcharvat@gmail.com>
2018-06-23 20:53:13 +0200

    enhancement/1 - material design setup

---
+++ app.js
@@ -0,0 +1,8 @@
+import {MDCNotchedOutline} from '@material/notched-outline';
+import {MDCTextField} from '@material/textfield';
+import {MDCRipple} from '@material/ripple';
+
+const notchedOutline = new MDCNotchedOutline(document.querySelector('.mdc-notched-outline'));
+const rwyOne = new MDCTextField(document.querySelector('.rwyOne'));
+const rwyTwo = new MDCTextField(document.querySelector('.rwyTwo'));
+const ripple = new MDCRipple(document.querySelector('.mdc-button'))
