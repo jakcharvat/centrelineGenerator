c0b42dd4795275d88485c2ca62865fd3c1848e55
jakcharvat <jakcharvat@gmail.com>
2018-07-13 18:35:28 +0200

    fix on

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = process.env = [{
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        PORT: JSON.stringify(process.env.PORT),
       },
     }),
   ],
