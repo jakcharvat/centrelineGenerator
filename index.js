aa6129f039a64057cd8ec8814c443f9862eb6efe
jakcharvat <jakcharvat@gmail.com>
2018-07-15 21:23:26 +0200

    further tests

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// const express = require('express')
+const express = require('express')
 // const path = require('path')
 const PORT = process.env.PORT || 8080
 
-// express()
+express()
 //     .use(express.static(path.join(__dirname, 'public')))
 //     .set('views', path.join(__dirname, 'views'))
 //     .set('view engine', 'ejs')
