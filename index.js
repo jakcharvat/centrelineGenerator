af08b230136e5b053739fe4dda74752384440b05
jakcharvat <jakcharvat@gmail.com>
2018-07-15 21:16:07 +0200

    further tests

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// const express = require('express')
-// const path = require('path')
-// const PORT = process.env.PORT || 8080
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 8080
 
 // express()
 //     .use(express.static(path.join(__dirname, 'public')))
